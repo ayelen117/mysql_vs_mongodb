a9801c956a704474be4f5bbfe13817d90e3df6c0
Ayelen Guerra <ayelen.guerra117@gmail.com>
2017-04-20 20:19:50 -0300

    * Modificación de migracion de creación de tabla users. * Se agregan tests de Mongo. * Creación de tablas de mongo/mysql.

---
+++ database/migrations/2017_03_21_003349_create_mongodb_tables.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use MongoDB\Client;
+
+class CreateMongodbTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $client = new Client();
+        $tables = ['categories', 'companies', 'company_currency', 'currencies', 'details', 'document_document',
+                   'document_transaction', 'documents', 'entities', 'entity_entity', 'fiscalpos', 'identifications',
+                   'inventories', 'pricelist_product', 'pricelists', 'products', 'receipts', 'responsibilities',
+                   'transactions'];
+
+        foreach ($tables as $table){
+            $client->tesis->createCollection(
+                $table,
+                array(
+                    'autoIndexId' => true,
+                    //                'validator' => true,
+                    //                'validationLevel' => true,
+                    //                'validationAction' => true,
+                )
+            );
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $client = new Client();
+        $tables = ['categories', 'companies', 'company_currency', 'currencies', 'details', 'document_document',
+                   'document_transaction', 'documents', 'entities', 'entity_entity', 'fiscalpos', 'identifications',
+                   'inventories', 'pricelist_product', 'pricelists', 'products', 'receipts', 'responsibilities',
+                   'transactions'];
+
+        foreach ($tables as $table) {
+            $client->tesis->$table->drop();
+        }
+    }
+}
--- tests/Mongo/UserTest.php
+++ tests/Mongo/UserTest.php
@@ -5,6 +5,7 @@ namespace tests\Mongo\UserTests;
 use App\Models\MongoDB\User as MongoUser;
 use App\Models\Mysql\User as MysqlUser;
 use tests\BaseTest;
+use MongoDB\Client;
 
 class UserTest extends \TestCase
 {
@@ -15,17 +16,110 @@ class UserTest extends \TestCase
         $objects = factory(MysqlUser::class, 100)->make();
         foreach ($objects as $var) {
             $tiempo_inicio = microtime(true);
-            MongoUser::create($var->toArray());
-//            $user = new MongoUser();
-//            $user->name = $var->name;
-//            $user->email = $var->email;
-//            $user->password = $var->password;
-//            $user->remember_token = $var->remember_token;
-//            $user->save();
+            $user = new MongoUser();
+            $user->name = $var->name;
+            $user->email = $var->email;
+            $user->password = $var->password;
+            $user->remember_token = $var->remember_token;
+            $user->save();
             $tiempo_fin   = microtime(true);
             $tiempo       = $tiempo_fin - $tiempo_inicio;
             $tiempo_total = $tiempo_total + $tiempo;
         }
         dump($tiempo_total);
+    }
+
+    public function test_if_user_insertOne_with_pure_mongo()
+    {
+        $client = new Client();
+        $collection = $client->tesis->users;
+
+        $tiempo_total = 0;
+        $objects = factory(MysqlUser::class, 1000)->make();
+        foreach ($objects as $var) {
+            $tiempo_inicio = microtime(true);
+
+            $collection->insertOne([
+                'name' => $var->name,
+                'email' => $var->email,
+                'password' => $var->password,
+                'remember_token' => $var->remember_token,
+            ]);
+
+            $tiempo_fin   = microtime(true);
+            $tiempo       = $tiempo_fin - $tiempo_inicio;
+            $tiempo_total = $tiempo_total + $tiempo;
+        }
+        dump($tiempo_total);
+    }
+
+    public function test_if_user_insertMany_with_pure_mongo()
+    {
+        $client = new Client();
+        $collection = $client->tesis->users;
+
+        $tiempo_total = 0;
+        $objects = factory(MysqlUser::class, 1000)->make()->toArray();
+        $tiempo_inicio = microtime(true);
+
+        $collection->insertMany($objects);
+
+        $tiempo_fin   = microtime(true);
+        $tiempo       = $tiempo_fin - $tiempo_inicio;
+        $tiempo_total = $tiempo_total + $tiempo;
+        dump($tiempo_total);
+    }
+
+    public function test_if_user_findOne_with_pure_mongo()
+    {
+        $client = new Client();
+        $collection = $client->tesis->users;
+
+        $tiempo_total = 0;
+        $tiempo_inicio = microtime(true);
+
+        $user = $collection->findOne();
+        $tiempo_fin   = microtime(true);
+        $tiempo       = $tiempo_fin - $tiempo_inicio;
+        $tiempo_total = $tiempo_total + $tiempo;
+        dump($tiempo_total);
+        dump($user);
+    }
+
+    public function test_if_user_find_with_pure_mongo()
+    {
+        $client = new Client();
+        $collection = $client->tesis->users;
+
+        $tiempo_total = 0;
+        $tiempo_inicio = microtime(true);
+
+        $users = $collection->find();
+        $tiempo_fin   = microtime(true);
+        $tiempo       = $tiempo_fin - $tiempo_inicio;
+        $tiempo_total = $tiempo_total + $tiempo;
+        dump($tiempo_total);
+    }
+
+    public function test_if_user_updateOne_with_pure_mongo()
+    {
+        $client = new Client();
+        $collection = $client->tesis->users;
+
+        $tiempo_total = 0;
+        $tiempo_inicio = microtime(true);
+
+        $updateResult = $collection->updateOne(
+            ['name' => ['$ne' => 'Test']],
+            ['$set' => ['name' => 'Aye']],
+            ['limit' => 1, 'upsert' => false]
+        );
+
+        $tiempo_fin   = microtime(true);
+        $tiempo       = $tiempo_fin - $tiempo_inicio;
+        $tiempo_total = $tiempo_total + $tiempo;
+        dump($tiempo_total);
+        printf("Matched %d document(s)\n", $updateResult->getMatchedCount());
+        printf("Modified %d document(s)\n", $updateResult->getModifiedCount());
     }
 }
